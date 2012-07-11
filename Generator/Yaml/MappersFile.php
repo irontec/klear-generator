5f1cbf3537dbd6265e55d59e7c041b1ab38f797a
Alayn Gortazar <zutoin@gmail.com>
2012-07-11 09:22:48 +0000

    Generando correctamente el mappersFile
    git-svn-id: http://dev.irontec.com/svn/generator/trunk/generator@37 df66723c-5fe6-4fa1-a1a8-62e9067b60c7

--- Generator/Yaml/MappersFile.php
+++ Generator/Yaml/MappersFile.php
@@ -3,6 +3,7 @@ class Generator_Yaml_MappersFile extends Generator_Yaml_AbstractConfig
 {
     public function __construct($tableList, $namespace)
     {
+        $mappers = array();
         foreach ($tableList as $table) {
             $camelCaseTable = Generator_Yaml_StringUtils::toCamelCase($table);
             $mapper = array(
@@ -11,10 +12,13 @@ class Generator_Yaml_MappersFile extends Generator_Yaml_AbstractConfig
                 'Sql',
                 ucfirst($camelCaseTable)
             );
-            $mappers['&' . $camelCaseTable] = array (
-                 'mapper' => Generator_Yaml_StringUtils::getMapperName($table, $namespace),
-                 'modelFile' => ucfirst($camelCaseTable)
+
+            $declaration = array(
+                 'mapper: ' . Generator_Yaml_StringUtils::getMapperName($table, $namespace),
+                 'modelFile: ' . ucfirst($camelCaseTable)
             );
+
+            $mappers[] = '&' . $camelCaseTable . ' {' . implode(',', $declaration) . '}';
         }
         $this->_data['mappers'] = $mappers;
     }
