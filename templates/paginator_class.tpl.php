9ccdadd936fb6f8267d15cdf6b13e258fe05787f
Alayn Gortazar <zutoin@gmail.com>
2013-04-03 12:12:36 +0000

    Poniendo las contrabarras delante de los namespaces de Zend
    git-svn-id: http://dev.irontec.com/svn/generator/trunk/generator@134 df66723c-5fe6-4fa1-a1a8-62e9067b60c7

--- templates/paginator_class.tpl.php
+++ templates/paginator_class.tpl.php
@@ -21,7 +21,7 @@ $namespace = !empty($this->_namespace) ? $this->_namespace . "\\" : "";
  * @author <?=$this->_author."\n"?>
  */
 namespace <?=$namespace?>Model;
-class Paginator extends Zend_Paginator_Adapter_DbSelect
+class Paginator extends \Zend_Paginator_Adapter_DbSelect
 {
     /**
      * Object mapper
@@ -36,7 +36,7 @@ class Paginator extends Zend_Paginator_Adapter_DbSelect
      * @param Zend_Db_Select $select The select query
      * @param <?=$namespace?>Mapper\Sql\Raw\MapperAbstract $mapper The mapper associated with the object type
      */
-    public function __construct(Zend_Db_Select $select, <?=$namespace?>Mapper\Sql\Raw\MapperAbstract $mapper)
+    public function __construct(\Zend_Db_Select $select, <?=$namespace?>Mapper\Sql\Raw\MapperAbstract $mapper)
     {
         $this->_mapper = $mapper;
         parent::__construct($select);
