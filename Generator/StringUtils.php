49920d309a2b33eccd92be6b4a723f74a4a1018f
ddniel16 <dani@irontec.com>
2016-08-04 13:10:32 +0200

    Cambio de "preg_replace" por "preg_replace_callback"
    * A partir de PHP 5.5.0 se emite un error de nivel E_DEPRECATED al pasar el modificador "\e". A partir de PHP 7.0.0, en este caso se emite un E_WARNING y el modificador "\e" no tiene efecto.

--- Generator/StringUtils.php
+++ Generator/StringUtils.php
@@ -1,13 +1,24 @@
 <?php
 class Generator_StringUtils
 {
+
     public static function toCamelCase($name, $type = 'lower')
     {
-        $camelCasedName = preg_replace('/[_|-](\w)/e', "strtoupper('\\1')", $name);
+
+        $camelCasedName = preg_replace_callback(
+            '/[_|-](\w)/',
+            function ($m) {
+                return strtoupper($m[1]);
+            },
+            $name
+        );
+
         if ($type == 'upper') {
             return ucfirst($camelCasedName);
         }
+
         return $camelCasedName;
+
     }
 
     public static function getMapperName($table, $namespace)
@@ -32,21 +43,21 @@ class Generator_StringUtils
 
         return '\\' . implode('\\', $class);
     }
-    
+
 /*
 1) Look at the last 2 letters in word, if last letter is "S" and 2nd to last letter is NOT "S", trim off "S"
 2) Look at end of word for "VES", if found, replace with "F"
 3) Look at end of word for "OES", if found, replace with "O"
 5) Look at end of word for "IES", if found, replace with "Y"
 6) Look at end of word for "XES", if found, replace with "X"
- */    
-    
+ */
+
     public static function getPlural($string)
     {
         $lastLetters = substr($string, -1);
-        
+
         $substitute = null;
-        
+
         switch ($lastLetters) {
             case 'f':
                 $substitute = 'ves';
@@ -58,28 +69,28 @@ class Generator_StringUtils
                 $substitute = 'ies';
                 break;
         }
-        
+
         if ($substitute) {
             return substr($string, 0, -1) . $substitute;
         }
-        
+
         return $string . 's';
-        
-        
+
+
     }
-    
-    
+
+
     public static function getSingular($string)
     {
         $lastLetters = substr($string, -2);
-        
+
         if ($lastLetters == 'ss'||$lastLetters == 'ws') {
             return $string;
         }
-        
+
         $lastLetters = substr($string, -3);
         $substitute = null;
-        
+
         switch ($lastLetters) {
             case 'ves':
                 $substitute = 'f';
@@ -94,22 +105,32 @@ class Generator_StringUtils
                 $substitute = 'x';
                 break;
         }
-        
+
         if ($substitute) {
             return substr($string, 0, -3) . $substitute;
         }
 
         $lastLetters = substr($string, -1);
-        
+
         if ($lastLetters == 's') {
             return substr($string, 0, -1);
         }
-        
+
         return $string;
     }
-    
+
     public static function getSentenceFromCamelCase($string)
     {
-        return trim(preg_replace("/([A-Z])/e", "strtolower(' \\1')", $string));
+
+        $string = preg_replace_callback(
+            "/([A-Z])/",
+            function ($m) {
+                return strtolower($m[1]);
+            },
+            $string
+        );
+
+        return trim($string);
+
     }
 }
