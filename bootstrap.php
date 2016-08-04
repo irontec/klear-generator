c99054fe7504f47a0d23b22e649d1b59e022add3
ddniel16 <dani@irontec.com>
2016-08-04 13:09:51 +0200

    añadir autoload si es una instalación por composer

--- bootstrap.php
+++ bootstrap.php
@@ -1,9 +1,16 @@
 <?php
-// Define application environment
-defined('APPLICATION_ENV')
-    || define('APPLICATION_ENV', (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production'));
+/**
+ * Define application environment
+ */
+$env = (getenv('APPLICATION_ENV') ? getenv('APPLICATION_ENV') : 'production');
+defined('APPLICATION_ENV') || define('APPLICATION_ENV', $env);
+
+$autoloadComposer = __DIR__ . '/../../autoload.php';
+if (file_exists($autoloadComposer)) {
+    require_once $autoloadComposer;
+}
 
 set_include_path(get_include_path() . PATH_SEPARATOR . __DIR__);
 require_once 'Zend/Loader/Autoloader.php';
-$loader = Zend_Loader_Autoloader::getInstance();
+$loader = \Zend_Loader_Autoloader::getInstance();
 $loader->registerNamespace('Generator');
