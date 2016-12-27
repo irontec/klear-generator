67577b4c6eea8ff674691d8fd6c5e27707f71d29
Javier Infante <jabi@irontec.com>
2016-12-27 10:15:16 +0100

    using view from Zend_Controller_Front if exists

--- templates/model_raw.tpl.php
+++ templates/model_raw.tpl.php
@@ -409,7 +409,13 @@ endforeach;
             'routeMap' => $routeMap
         );
 
-        $view = new \Zend_View();
+        if (\Zend_Controller_Front::getInstance()) {
+            $view = \Zend_Controller_Front::getInstance()
+                ->getParam('bootstrap')
+                ->getResource('view');
+        } else {
+            $view = new \Zend_View();
+        }
         $fsoUrl = $view->serverUrl($view->url($route, 'fso'));
 
         return $fsoUrl;
